6764042d48bfbff9189c7c4435182c31efb4950f
shacovik <shacovik@gmail.com>
2022-01-24 16:33:55 -0600

    first commit

---
+++ js/main.js
@@ -0,0 +1,36 @@
+import Game from "./game.js"
+import GameView from "./GameView.js"
+
+let game = new Game();
+let gameView = new GameView();
+
+
+document.querySelector(".restart")
+.addEventListener("click", () => {
+    onRestartClick();
+})
+
+let tiles = document.querySelectorAll(".board-tile");
+console.log(tiles);
+tiles.forEach((tile) => {
+    tile.addEventListener("click", () => {
+        onTileClick(tile.dataset.index);
+
+    })
+})
+
+
+function onTileClick(i){
+    // do something
+    game.makeMove(i)
+    gameView.updateBoard(game);
+
+}
+
+function onRestartClick(){
+    game = new Game();
+    gameView.updateBoard(game);
+}
+
+
+gameView.updateBoard(game);
---
+++ plan.md
@@ -0,0 +1,7 @@
+# TODO Tasks
+
+1. Create the html for the Tic Tac Toe game.
+2. Add functionality to start a new game. 
+3. Add functionality to display your choice in the grid
+4. Add funtionality to switch/change turns between players. 
+5. Add functionality to determine winner. 
